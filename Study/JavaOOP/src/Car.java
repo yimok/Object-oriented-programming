2166c507fb01aaf08069a16c90dfd42f4f8785aa
yim hyo kyun <dlagyrbs@koreatech.ac.kr>
2017-01-03 10:51:59 +0900

    update

---
+++ Study/JavaOOP/src/Car.java
@@ -0,0 +1,19 @@
+
+public class Car {
+
+	static int speed;// class member
+	int fuel;
+	
+	//생성자는 리턴타입이 없어야함.
+	public Car()
+	{
+		//Default constructor
+	}
+	
+	public void go()  
+	{
+		int a = 100;
+		System.out.println("전진합니다!");
+	}
+	
+}
